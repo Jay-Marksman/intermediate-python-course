3fd194d06ac91bcfb4d24d974e7939fb76af2850
Jay-Marksman <JayMarksman@protonmail.com>
2021-01-01 15:15:31 -0500

    Added flavor

--- dice_roller.py
+++ dice_roller.py
@@ -6,7 +6,12 @@ die_sum = 0
 for i in range(0,2):
   roll = random.randint(1,100)
   die_sum += roll
-  print(f'You rolled a(n) {roll}')
+  if roll == 1:
+   print(f'!!!!!!Natura{roll} Wonders!!!!!!')
+  elif roll == 100:
+   print(f'!!!!!!!Maximum Success!!{roll}!!')
+  else:
+   print(f'You rolled a(n) {roll}')
 print(f'You have rolled a total of {die_sum}')
 
 if __name__== "__main__":
