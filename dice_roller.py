e46a97850dcbd2c1551a208278be868812e8eb10
Jay-Marksman <JayMarksman@protonmail.com>
2021-01-01 14:41:29 -0500

    Roll 2 onehundred-sided die and total

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+import random
 def main():
-  print('You rolled a die')
+# roll two onehundred-sided die and total 
+  die = 2
+die_sum = 0
+for i in range(0,2):
+  roll = random.randint(1,100)
+  die_sum += roll
+  print(f'You rolled a(n) {roll}')
+print(f'You have rolled a total of {die_sum}')
 
 if __name__== "__main__":
   main()
