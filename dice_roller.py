b3d2bd36b881e09c2bf73019bf3b00d804e61afc
Jay-Marksman <JayMarksman@protonmail.com>
2021-01-01 17:10:12 -0500

    Natural Wonders Dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,18 +1,18 @@
 import random
-def main():
-# roll two onehundred-sided die and total 
-  die = 2
-die_sum = 0
-for i in range(0,2):
-  roll = random.randint(1,100)
-  die_sum += roll
+def main ():
+ dice_rolls = int(input('How many dice would you like to roll? '))
+ dice_size = int(input('How many sides are the dice? '))
+ dice_sum = 0
+ for i in range(0,dice_rolls):
+  roll = random.randint(1,dice_size)
+  dice_sum += roll
   if roll == 1:
-   print(f'!!!!!!Natura{roll} Wonders!!!!!!')
-  elif roll == 100:
-   print(f'!!!!!!!Maximum Success!!{roll}!!')
+    print(f'!!!!Natura{roll} Wonders!!!!')
+  elif roll == dice_size:
+    print(f'!!!!Great Success!!!!')
   else:
-   print(f'You rolled a(n) {roll}')
-print(f'You have rolled a total of {die_sum}')
+    print(f'You rolled a(n) {roll}')
+ print(f'For a grand total of {dice_sum}')
 
 if __name__== "__main__":
   main()
